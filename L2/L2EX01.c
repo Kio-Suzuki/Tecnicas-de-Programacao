84564e536f4b5b9feb4740073d0dd03f0cc4a23e
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2022-10-26 16:08:03 -0300

    Exercícios 1 e 2 da lista 2

--- L2/L2EX01.c
+++ L2/L2EX01.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
 
+int verificaMaximo();
+
+int main()
+{
+    int x[10];
+    for (int i = 0; i < 10; i++){
+        printf ("Digite um valor: ");
+        scanf ("%d", &x[i]);
+    }
+    printf ("O maior valor do vetor = %d", verificaMaximo(x));
+    return 0;
+}
+
+int verificaMaximo(int x[])
+{
+    int y;
+    for (int i = 0; i < 10; i++){
+        if (x[i] > x[i+1]){
+            y = x[i];
+        }else{
+            y = x[i+1];
+        }
+    }
+    return y;
+}
