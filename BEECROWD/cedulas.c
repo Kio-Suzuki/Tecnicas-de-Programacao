c58c3995c4a5e7a160a91430f66186760bd32532
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2022-11-12 15:58:17 -0300

    bee

---
+++ BEECROWD/cedulas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+    int N;
+    scanf ("%d", &N);
+    if (N >= 100){
+        C = N / 100;
+    }else if(N >= 50){
+        D = N / 50;
+    }else if(N >= 20){
+        DU= N / 20;
+    }else if(N >= 10){
+        
+    }
+    printf ("%d\n", );
+    printf ("%d nota (s) de R$ 100,00\n", );
+    printf ("%d nota (s) de R$ 50,00\n", );
+    printf ("%d nota (s) de R$ 20,00\n", );
+    printf ("%d nota (s) de R$ 10,00\n", );
+    printf ("%d nota (s) de R$ 2,00\n", );
+    printf ("%d nota (s) de R$ 1,00\n", );
+    return 0;
+}
--- TESTES/ConvTempo.c
+++ TESTES/ConvTempo.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
  
 int main() {
-    int N, m, h, s;
-    scanf ("%d", &N);
-    if (N % 6 == 0){
-        m=N/60;
-    }else{
-
-    }
-    if (m > 60){
-        h=m/60;
-    }else{
-        h=0;
-    }
-    h=m/60;
-    printf ("N / 6 = %d\n", N%6);
-    printf ("%d:%d:%d/n", h, m, s);
+    int X, Y;
+    float T;
+    scanf ("%d %d", &X, &Y);
+    T = X*Y/12.0;
+    printf ("%.3f\n", T);
     return 0;
 }
