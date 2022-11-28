4ee882f934a9c6b159dd9d6f623ff1e2f3109dd8
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2022-11-28 18:44:22 -0300

    create gerarCodigo

--- TESTES/gerarCodigo.c
+++ TESTES/gerarCodigo.c
@@ -11,35 +11,35 @@ int main()
 void gerarCodigo()
 {
     int cod[10][10], i, j, cont = 0; 
-    char id[10];
+    char id[11] = "0000000000";
     srand(time(NULL));
     for (i = 0; i < 10; i++){                                      
         for (j = 0; j < 10; j++){                              
             cod[i][j] = rand() %2;                               
         } 
-    }  
+    }
+    for (i = 0; i < 10; i++){                                      
+        for (j = 0; j < 10; j++){                              
+            printf ("%d ", cod[i][j]);                              
+        }
+        printf ("\n");
+    }   
+    printf ("\nCodigo:\n");
     for (i = 0; i < 10; i++){
         for (j = 0; j < 10; j++){
             if (cod[j][i] == 1){
                 cont++;
-            }      
+            }   
+            if (cont >= 5){
+            id[i] = '1';
+            }else{
+            id[i] = '0';
+            cont = 0;
+            printf ("id[%d] = %s\n", i, id);  
+            }   
         }
-        if (cont >= 5){
-            id[i] = 1;
-        }else{
-            id[i] = 0;
-        }
-        cont = 0;  
     }
-    for (i = 0; i < 10; i++){                                      
-        for (j = 0; j < 10; j++){                              
-            printf ("%d", cod[i][j]);                              
-        }
-        printf ("\n");
-    } 
-    printf ("\nCodigo:\n");
-
-    puts (id);
     
-     
+    printf ("codigo na string = %s", id); 
 }
+    
---
+++ TESTES/string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    char senha[6];
+    int i;
+    for (int i = 0; i < 6; i++)
+        printf ("Defina a letra %d", i+1);
+        scanf ("%c", senha[i]);
+}
